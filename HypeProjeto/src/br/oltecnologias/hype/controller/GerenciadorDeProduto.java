4280f881ed4f27b73328178d1c772db6c5abf6a8
Odravison <Odravison@Odravison-PC.home>
2015-09-29 16:39:19 -0300

    Alteração no método de getDisponibilidadeDeProduto.

--- HypeProjeto/src/br/oltecnologias/hype/controller/GerenciadorDeProduto.java
+++ HypeProjeto/src/br/oltecnologias/hype/controller/GerenciadorDeProduto.java
@@ -418,17 +418,16 @@ public class GerenciadorDeProduto {
         return retorno;
     }
 
-    public boolean consultarDisponibilidadeDeProdutoEntreDatas(Calendar dataInicio, Calendar dataFiinal, String codigoProduto) {
+    public Produto consultarDisponibilidadeDeProdutoEntreDatas(Calendar dataInicio, Calendar dataFiinal, String codigoProduto) {
         List<Produto> produtos = this.getProdutosDisponiveisEntreDatas(dataInicio, dataFiinal);
+        Produto peFail = null;
         for (Produto pe : produtos) {
             if (pe.getCodigo().toUpperCase().equals(codigoProduto.toUpperCase())) {
-                if (pe.getQuantidade() >= 1) {
-                    return true;
-                }
+                return pe;
             }
         }
 
-        return false;
+        return peFail;
     }
 
     public List<Produto> getProdutosParaCostureira() throws ProdutoInexistenteException {
